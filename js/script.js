fdfa5015d4b6bff307632842cb140a85c34d3c2e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-10-14 08:52:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-/**/
+/* ======= JavaScript ======= */
 let teacherForms = document.getElementsByClassName("teacher-contract");
 
 for(let teacherForm of teacherForms){
     teacherForm.addEventListener("submit", function(event) {
         event.preventDefault();
-        console.log("executou o submit");
+        //console.log("executou o submit");
+
+        let price = teacherForm.price.value;
+        let hours = teacherForm.hours.value;
+
+        let total = price * hours;
+
+        console.log(total);
     });
 }
 
