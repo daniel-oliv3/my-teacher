c06482698e3c14b330527d1b71788b39b62e3b32
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-10-14 08:42:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,97 +1,20 @@
-/* ======= JavaScript basic ======= */
-//Comando exibe informações no console
-console.log("Olá, Mundo!");
+/**/
+let teacherForms = document.getElementsByClassName("teacher-contract");
 
-// ======= Variaveis e tipos de dados =======
-/* String */
-let nome = "Daniel Oliveira";
-console.log(nome);
-
-/* Number */
-let idade = 35;
-console.log(idade);
-
-/* Boolean */
-let ehprogramador = true;
-console.log(ehprogramador);
-
-// ======= Operadores matematicos =======
-let num1 = 10;
-let num2 = 5;
-
-/* Soma */
-console.log("Soma");
-console.log(num1 + num2);
-
-/* Subtração */
-console.log("Subtração");
-console.log(num1 - num2);
-
-/* Multiplicação */
-console.log("Multiplicação");
-console.log(num1 * num2);
-
-/* Divisão */
-console.log("Divisão");
-console.log(num1 / num2);
-
-/* Modulo/Resto da divisão */
-console.log("Modulo/Resto da divisão");
-console.log(num1 % num2);
-
-/* ======= Arrays ======= */
-let notas = [10, 8.9, 7.3, 8.5];
-console.log(notas);
-console.log(notas[0]);
-console.log(notas[2]);
-
-notas[0] = 8;
-console.log(notas);
-
-notas.push(9);
-console.log(notas);
-
-
-/* ======= Laços de repetição ======= */
-// For
-let numeros = [1,2,3,4,5,6,7];
-for(let i = 0; i < 7; i++){
-    console.log(numeros[i]);
+for(let teacherForm of teacherForms){
+    teacherForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        console.log("executou o submit");
+    });
 }
 
-console.log("Terminou o for");
-
-// For of
-for(let nota of notas){
-    console.log(nota);
-}
-
-console.log("Terminou o for of");
-
-/* ======= Objetos javascript ======= */
-let aluno = {
-    nome: "Daniel Oliveira",
-    idade: 35,
-    notas: [10, 8.9, 7.3, 8.5],
-    contato: { 
-        telefone: "(55)92-99131-5030",
-        email: "danieloliveira.webmaster@gmail.com"
-    }
-}
-
-console.log(aluno);
-console.log(aluno.nome);
-console.log(aluno.idade);
-console.log(aluno.contato);
-console.log(aluno.contato.email);
-console.log(aluno.contato.telefone);
 
 
-/* ======= DOM ======= */
-let elemento = document.getElementById("main-container"); //<main id="main-container"></main>
-console.log(elemento);
 
-elemento.innerHTML = "Alterando via JS";
+
+
+
+
 
 
 
